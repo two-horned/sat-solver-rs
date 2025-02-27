1a8ab931b5bf87bbc5ed4ddc90be6794f6cd48fc
Said Kadrioski <said@kadrioski.de>
2025-02-27 14:17:09 +0100

    Formatty1

--- src/alloc.rs
+++ src/alloc.rs
@@ -4,53 +4,58 @@ use core::slice;
 use std::alloc::{alloc, dealloc};
 use std::sync::Mutex;
 
-
 impl PoolAlloc {
-  pub fn new(layout: Layout, capacity: usize) -> Self {
-    let objalg = layout.align();
-    let (memlyt, objsiz) = layout.repeat(capacity).unwrap();
-    let memory = unsafe { alloc(memlyt) };
-    let frptrs = Mutex::new(
-      (0..capacity).map(|x| unsafe { memory.add(x * objsiz) }).collect()
-    );
-    Self { objsiz, objalg, frptrs, memlyt, memory }
-  }
+    pub fn new(layout: Layout, capacity: usize) -> Self {
+        let objalg = layout.align();
+        let (memlyt, objsiz) = layout.repeat(capacity).unwrap();
+        let memory = unsafe { alloc(memlyt) };
+        let frptrs = Mutex::new(
+            (0..capacity)
+                .map(|x| unsafe { memory.add(x * objsiz) })
+                .collect(),
+        );
+        Self {
+            objsiz,
+            objalg,
+            frptrs,
+            memlyt,
+            memory,
+        }
+    }
 }
-
 
 unsafe impl Sync for PoolAlloc {}
 
-
 impl Drop for PoolAlloc {
-  fn drop(&mut self) { unsafe { dealloc(self.memory, self.memlyt) } }
+    fn drop(&mut self) {
+        unsafe { dealloc(self.memory, self.memlyt) }
+    }
 }
-
 
 pub struct PoolAlloc {
-  objsiz: usize,
-  objalg: usize,
-  frptrs: Mutex<Vec<*mut u8>>,
-  memlyt: Layout,
-  memory: *mut u8
+    objsiz: usize,
+    objalg: usize,
+    frptrs: Mutex<Vec<*mut u8>>,
+    memlyt: Layout,
+    memory: *mut u8,
 }
 
-
 unsafe impl Allocator for PoolAlloc {
-  fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-    let (align, size) = (layout.align(), layout.size());
-    if align > self.objalg || size > self.objsiz {
-      return Err(AllocError)
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        let (align, size) = (layout.align(), layout.size());
+        if align > self.objalg || size > self.objsiz {
+            return Err(AllocError);
+        }
+        let mut frptrs = self.frptrs.lock().unwrap();
+        if let Some(start) = frptrs.pop() {
+            let slice = unsafe { slice::from_raw_parts_mut(start, size) };
+            return Ok(unsafe { NonNull::new_unchecked(slice) });
+        }
+        Err(AllocError)
     }
-    let mut frptrs = self.frptrs.lock().unwrap();
-    if let Some(start) = frptrs.pop() {
-      let slice = unsafe { slice::from_raw_parts_mut(start, size) };
-      return Ok(unsafe { NonNull::new_unchecked(slice) })
-    }
-    Err(AllocError)
-  }
 
-  unsafe fn deallocate(&self, ptr: NonNull<u8>, _: Layout) {
-    let mut frptrs = self.frptrs.lock().unwrap();
-    frptrs.push(ptr.as_ptr());
-  }
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, _: Layout) {
+        let mut frptrs = self.frptrs.lock().unwrap();
+        frptrs.push(ptr.as_ptr());
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(allocator_api, ptr_sub_ptr, alloc_layout_extra, iter_chain)]
 
-
 pub mod alloc;
 pub mod data;
 pub mod solver;
