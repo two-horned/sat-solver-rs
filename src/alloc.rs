e6b93da41cc6f12f26a70e9593f583a59e4a8b0c
Said Kadrioski <said@kadrioski.de>
2025-03-10 00:19:16 +0100

    Add new allocator.

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,10 +1,11 @@
 use core::alloc::{AllocError, Allocator, Layout};
 use core::ptr::{self, NonNull};
 use std::alloc::{alloc, dealloc};
+use std::collections::BinaryHeap;
 use std::sync::Mutex;
 
 impl PoolAlloc {
-    pub fn new(layout: Layout, capacity: usize) -> Self {
+    pub(crate) fn new(layout: Layout, capacity: usize) -> Self {
         let objalg = layout.align();
         let (memlyt, objsiz) = layout.repeat(capacity).unwrap();
         let memory = unsafe { alloc(memlyt) };
@@ -13,6 +14,7 @@ impl PoolAlloc {
                 .map(|x| unsafe { memory.add(x * objsiz) })
                 .collect(),
         );
+
         Self {
             objsiz,
             objalg,
@@ -31,8 +33,7 @@ impl Drop for PoolAlloc {
     }
 }
 
-#[derive(Debug)]
-pub struct PoolAlloc {
+pub(crate) struct PoolAlloc {
     objsiz: usize,
     objalg: usize,
     frptrs: Mutex<Vec<*mut u8>>,
@@ -42,13 +43,12 @@ pub struct PoolAlloc {
 
 unsafe impl Allocator for PoolAlloc {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let (align, size) = (layout.align(), layout.size());
-        if align > self.objalg || size > self.objsiz {
+        if layout.align() > self.objalg || layout.size() > self.objsiz {
             return Err(AllocError);
         }
         let mut frptrs = self.frptrs.lock().unwrap();
         if let Some(start) = frptrs.pop() {
-            let ptr = ptr::from_raw_parts_mut(start, size);
+            let ptr = ptr::from_raw_parts_mut(start, layout.size());
             return Ok(unsafe { NonNull::new_unchecked(ptr) });
         }
         Err(AllocError)
@@ -58,4 +58,90 @@ unsafe impl Allocator for PoolAlloc {
         let mut frptrs = self.frptrs.lock().unwrap();
         frptrs.push(ptr.as_ptr());
     }
+}
+
+impl StacklikeAlloc {
+    pub(crate) fn new(layout: Layout) -> Self {
+        let memlyt = layout.pad_to_align();
+
+        Self {
+            contrl: Mutex::new(StacklikeCtrl::new()),
+            memlyt,
+            memory: unsafe { alloc(memlyt) },
+        }
+    }
+}
+
+unsafe impl Sync for StacklikeAlloc {}
+
+impl Drop for StacklikeAlloc {
+    fn drop(&mut self) {
+        unsafe { dealloc(self.memory, self.memlyt) }
+    }
+}
+
+unsafe impl Allocator for StacklikeAlloc {
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        if layout.align() > self.memlyt.align() {
+            return Err(AllocError);
+        }
+
+        let mut contrl = self.contrl.lock().unwrap();
+        let mask = layout.align() - 1;
+        let start = (contrl.offset + mask) & !mask;
+        let end = start + layout.size();
+
+        if end > self.memlyt.size() {
+            return Err(AllocError);
+        }
+
+        let ptr = ptr::from_raw_parts_mut(unsafe { self.memory.add(start) }, layout.size());
+        {
+            let offset = contrl.offset;
+            contrl.alptrs.push(offset);
+        }
+        contrl.offset = end;
+        Ok(unsafe { NonNull::new_unchecked(ptr) })
+    }
+
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        let mut contrl = self.contrl.lock().unwrap();
+
+        {
+            let offset = unsafe { ptr.as_ptr().offset_from_unsigned(self.memory) + layout.size() };
+            contrl.frptrs.push(offset);
+        }
+
+        while let Some(&x) = contrl.frptrs.peek() {
+            assert!(x <= contrl.offset);
+            if x < contrl.offset {
+                break;
+            }
+
+            contrl.offset = contrl.alptrs.pop().unwrap();
+            contrl.frptrs.pop();
+        }
+    }
+}
+
+pub(crate) struct StacklikeAlloc {
+    contrl: Mutex<StacklikeCtrl>,
+    memlyt: Layout,
+    memory: *mut u8,
+}
+
+impl StacklikeCtrl {
+    fn new() -> Self {
+        Self {
+            offset: 0,
+            alptrs: Vec::new(),
+            frptrs: BinaryHeap::new(),
+        }
+    }
+}
+
+pub(crate) struct StacklikeCtrl {
+    offset: usize,
+    alptrs: Vec<usize>,
+    frptrs: BinaryHeap<usize>,
 }
